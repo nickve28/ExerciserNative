acef65ac4d82e10e943cc3bc91556cdec4150aa3
nickve28 <nickvaneyk@gmail.com>
2018-10-20 13:04:03 +0200

    Add typescript

--- containers/App.tsx
+++ containers/App.tsx
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
+import { Component } from "react";
 import { AsyncStorage } from "react-native";
 import ApolloClient from 'apollo-client';
 import { InMemoryCache } from "apollo-cache-inmemory";
